8b2695c3c62e8e2e514510bf1cffe2961d35d6e8
Shlx <magicmarvin@hotmail.de>
2018-06-17 18:16:35 +0200

    Cleaned up fetch_data_factory, added documentation

--- src/app/pages/kevin/fetch-data-factory.js
+++ src/app/pages/kevin/fetch-data-factory.js
@@ -1,148 +1,38 @@
+const Property = {
+    DEPTH:              "depth",
+    EFFICIENCY:         "efficiency",
+    ELECTRICAL_POWER:   "electrical_power",
+    EXTRACTION_RATE:    "extraction_rate",
+    GEOTHERMAL_POWER:   "geothermal_power",
+    ID:                 "id",
+    TEMPERATURE:        "temperature",
+    TIME_STEP:          "time_step",
+    NAME:               "name"
+};
+
 (function () {
     'use strict';
 
     angular.module('Geothermal.pages.kevin')
-        .factory('fetchDataFactory',function() {
+        .factory('fetchDataFactory', function() {
 
             return {
+
+                // Fetch the data of a geothermal power plant. Returns an array
+                // of JSON objects. (See 'assets' folder for the exact structure)
                 fetchData: async function(filename) {
                     return await fetch('../../../assets/geothermal-data/' + this.toJsonPath(filename))
                         .then(response => response.json());
                 },
 
-                fetchTimeStepArray: async function(filename){
+                // Fetch each nth entry of the data.
+                fetchDownSampledData: async function(filename, n) {
                     let data = await this.fetchData(filename);
-                    let timeStepArray = [];
-                    data.forEach(function (value) {
-                        timeStepArray.push(value['time_step']);
-                    });
-                    return timeStepArray;
+                    return this.downsample(data, n);
                 },
 
-                fetchEfficiencyArray: async function(filename){
-                    let data = await this.fetchData(filename);
-                    let efficiencyArray = [];
-                    data.forEach(function (value) {
-                        efficiencyArray.push(parseFloat(value['efficiency']));
-                    });
-                    return efficiencyArray;
-                },
-
-                fetchEfficiencyArrayInPercent: async function(filename){
-                    let data = await this.fetchData(filename);
-                    let efficiencyArray = [];
-                    data.forEach(function (value) {
-                        efficiencyArray.push(parseFloat(value['efficiency']) * 100);
-                    });
-                    return efficiencyArray;
-                },
-
-                fetchDownSampledData: async function(filename, n){
-                    let data = await this.fetchData(filename);
-                    let downSampledArray = [];
-                    for (let i = 0; i < data.length; i = i + n){
-                        downSampledArray.push(data[i]);
-                    }
-                    return downSampledArray;
-                },
-
-                fetchDownSampledTimeArray: async function(filename, n){
-                    let timeSteps = await this.fetchTimeStepArray(filename);
-                    let downSampledArray = [];
-                    for (let i = 0; i < timeSteps.length; i = i + n){
-                        downSampledArray.push(data[i]);
-                    }
-                    return downSampledArray;
-                },
-
-                fetchDownSampledEfficiencyArray: async function(filename, n){
-                    let efficiencies = await this.fetchEfficiencyArray(filename);
-                    let downSampledArray = [];
-                    for (let i = 0; i < efficiencies.length; i = i + n){
-                        downSampledArray.push(data[i]);
-                    }
-                    return downSampledArray;
-                },
-
-                fetchExtractionRatesArray: async function(filename){
-                    let data = await this.fetchData(filename);
-                    let extractionRates = [];
-                    data.forEach(function (value) {
-                        extractionRates.push(parseFloat(value['extraction_rate']));
-                    });
-                    return extractionRates;
-                },
-
-                fetchDownSampledExtractionRatesArray: async function(filename, n){
-                    let data = await this.fetchExtractionRatesArray(filename);
-                    let downSampledArray = [];
-                    for (let i = 0; i < data.length; i = i + n){
-                        downSampledArray.push(data[i]);
-                    }
-                    return downSampledArray;
-                },
-
-                fetchElectricalPowerArray: async function(filename){
-                    let data = await this.fetchData(filename);
-                    let electricalPowerArray = [];
-                    data.forEach(function (value) {
-                        electricalPowerArray.push(parseFloat(value['electrical_power']));
-                    });
-                    return electricalPowerArray;
-                },
-
-                fetchDownSampledElectricalPowerArray: async function(filename, n){
-                    let data = await this.fetchElectricalPowerArray(filename);
-                    let downSampledArray = [];
-                    for (let i = 0; i < data.length; i = i + n){
-                        downSampledArray.push(data[i]);
-                    }
-                    return downSampledArray;
-                },
-
-                fetchDepth: async function(filename){
-                    let data = await this.fetchData(filename);
-                    return data[0]['depth'];
-                },
-
-                fetchTemperature: async function(filename){
-                    let data = await this.fetchData(filename);
-                    return data[0]['temperature'];
-                },
-
-                fetchGeothermalPowerArray: async function(filename){
-                    let data = await this.fetchData(filename);
-                    let geothermalPowerArray = [];
-                    data.forEach(function (value) {
-                        geothermalPowerArray.push(parseFloat(value['geothermal_power']));
-                    });
-                    return geothermalPowerArray;
-                },
-
-                fetchDownSampledGeothermalPowerArray: async function(filename, n){
-                    let data = await this.fetchGeothermalPowerArray(filename);
-                    let downSampledArray = [];
-                    for (let i = 0; i < data.length; i = i + n){
-                        downSampledArray.push(data[i]);
-                    }
-                    return downSampledArray;
-                },
-
-                fetchGeoData: async function(){
-                    return await fetch('../../../assets/geothermal-data/general-data/geo-data.json')
-                        .then(response => response.json());
-                },
-
-                getAllNames: async function(){
-                    let geoData = await this.fetchGeoData();
-                    let nameArray = [];
-                    geoData.forEach(function (value) {
-                        nameArray.push(value['name']);
-                    });
-                    return nameArray;
-                },
-
-                fetchAllData: async function(){
+                // Fetch the data for every power plant.
+                fetchAllData: async function() {
                     let names = await this.getAllNames();
                     let dataArray = [];
                     names.forEach(async function (value) {
@@ -154,20 +44,146 @@
                     return dataArray;
                 },
 
-                toJsonPath: function(filename){
-                    return filename + ".json";
+
+                // Fetch the depth of a power plant. This returns a single value
+                // since the station's depth is constant.
+                fetchDepth: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return data[0][Property.DEPTH];
                 },
 
+
+                // Fetch the efficiency of each entry of a power plant.
+                fetchEfficiencyArray: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return this.getProperty(data, Property.EFFICIENCY);
+                },
+
+                // Fetch a downsampled efficiency array.
+                fetchDownSampledEfficiencyArray: async function(filename, n) {
+                    let efficiencies = await this.fetchEfficiencyArray(filename);
+                    return this.downsample(efficiencies, n);
+                },
+
+                // Fetch the efficiency of each entry as a percentage.
+                fetchEfficiencyArrayInPercent: async function(filename) {
+                    let data = await this.fetchEfficiencyArray(filename);
+                    return data.map(function(x) { return x * 100; });
+                },
+
+                // Fetch a downsampled efficiency array in percentages.
+                fetchDownSampledEfficiencyArrayInPercent: async function(filename, n) {
+                    let efficiencies = await this.fetchEfficiencyArrayInPercent(filename);
+                    return this.downsample(efficiencies, n);
+                },
+
+
+                // Fetch the electrical power pf each entry of a power plant.
+                fetchElectricalPowerArray: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return this.getProperty(data, Property.ELECTRICAL_POWER);
+                },
+
+                // Fetch a downsampled electrical power array.
+                fetchDownSampledElectricalPowerArray: async function(filename, n) {
+                    let data = await this.fetchElectricalPowerArray(filename);
+                    return this.downsample(data, n);
+                },
+
+
+                // Fetch the extraction rate of each entry of a power plant.
+                fetchExtractionRatesArray: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return this.getProperty(data, Property.EXTRACTION_RATE);
+                },
+
+                // Fetch a downsampled extraction rate array.
+                fetchDownSampledExtractionRatesArray: async function(filename, n) {
+                    let data = await this.fetchExtractionRatesArray(filename);
+                    return this.downsample(data, n);
+                },
+
+
+                // Fetch the geothermal power of each entry of a power plant.
+                fetchGeothermalPowerArray: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return this.getProperty(data, Property.GEOTHERMAL_POWER);
+                },
+
+                // Fetch a downsampled geothermal power array.
+                fetchDownSampledGeothermalPowerArray: async function(filename, n) {
+                    let data = await this.fetchGeothermalPowerArray(filename);
+                    return this.downsample(data, n);
+                },
+
+
+                // Fetch the temperature of a power plant. This returns a single value
+                // since the station's water temperature is constant.
+                fetchTemperature: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return data[0][Property.TEMPERATURE];
+                },
+
+
+                // Fetch the timestamps of each entry of a power plant.
+                fetchTimeStepArray: async function(filename) {
+                    let data = await this.fetchData(filename);
+                    return this.getProperty(data, Property.TIME_STEP);
+                },
+
+                // Fetch a downsampled timestamp array.
+                fetchDownSampledTimeArray: async function(filename, n) {
+                    let timeSteps = await this.fetchTimeStepArray(filename);
+                    return this.downsample(timeSteps, n);
+                },
+
+
+                // Fetch the geographical location of all power plants.
+                fetchGeoData: async function() {
+                    return await fetch('../../../assets/geothermal-data/general-data/geo-data.json')
+                        .then(response => response.json());
+                },
+
+                // Get the name of every power plant.
+                getAllNames: async function() {
+                    let geoData = await this.fetchGeoData();
+                    return this.getProperty(geoData, Property.NAME);
+                },
+
+                // Fetch an array of each power plant's name and it's respective depth.
                 fetchNameDepthArray: async function () {
                     let nameDepthArray = [];
                     let nameArray = await this.getAllNames();
-                    for (let i = 0; i < nameArray.length; i++){
+                    for (let i = 0; i < nameArray.length; i++) {
                         let tmpArray = [];
                         tmpArray.push(nameArray[i]);
                         tmpArray.push(await this.fetchDepth(nameArray[i]));
                         nameDepthArray.push(tmpArray);
                     }
                     return nameDepthArray;
+                },
+
+                // Get the JSON path of a power plant.
+                toJsonPath: function(filename) {
+                    return filename + ".json";
+                },
+
+                // Downsample a set of data, so that only each nth element is kept.
+                downsample: async function (data, n) {
+                    let downSampledArray = [];
+                    for (let i = 0; i < data.length; i = i + n) {
+                        downSampledArray.push(data[i]);
+                    }
+                    return downSampledArray;
+                },
+
+                // Filter a set of data, so that only a single property is kept.
+                getProperty: async function (data, property) {
+                    let array = [];
+                    data.forEach(function (value) {
+                        array.push(value[property]);
+                    });
+                    return array;
                 }
             }
         });
--- src/app/pages/marvin/marvin.js
+++ src/app/pages/marvin/marvin.js
@@ -3,8 +3,33 @@
 
     angular.module('Geothermal.pages.marvin')
         .controller('MarvinCtrl',
-            ['$scope', 'fetchDataFactory',
-                async function MarvinCtrl($scope, fetchDataFactory) {
-                    let data = await fetchDataFactory.fetchData('1d8a69a5-b692-47b7-aacb-b7f26692c0ec');
-                    console.log(data);
-                }])})();
+            ['$scope', 'fetchDataFactory', 'baConfig', '$timeout',
+
+                async function MarvinCtrl($scope, fetchDataFactory, baConfig, $timeout) {
+
+                    let file = '1d8a69a5-b692-47b7-aacb-b7f26692c0ec';
+                    let data = await fetchDataFactory.fetchData(file);
+                    console.log(fetchDataFactory.fetchNameDepthArray(file));
+
+                    $scope.simpleLineOptions = {
+                        color: baConfig.colors.defaultText,
+                        fullWidth: true,
+                        height: "300px",
+                        chartPadding: {
+                            right: 40
+                        }
+                    };
+
+                    $scope.simpleLineData = {
+                        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+                        series: [
+                            [34, 20, 13, 45, 50],
+                            [10, 45, 30, 14, 12],
+                            [34, 12, 12, 40, 50],
+                            [10, 43, 25, 22, 16],
+                            [3, 6, 10, 33, 43]
+                        ]
+                    };
+
+                }])
+})();
